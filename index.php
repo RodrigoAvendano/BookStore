249fb15c50be4ba3286e176ad621d1ffddbb2d71
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2023-06-07 18:15:18 -0600

    Feat: Add index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore - Tienda en línea</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/44a52db869.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>Hola</h1>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
