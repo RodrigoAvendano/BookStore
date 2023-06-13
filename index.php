b23124e4e6ead4853f305d2bea328e5a5805ee17
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2023-06-13 17:28:45 -0600

    Feat: Add header's HTML and css style

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BookStore - Tienda en línea</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <title>BookStore - Tienda en línea</title>
     <script src="https://kit.fontawesome.com/44a52db869.js" crossorigin="anonymous"></script>
 </head>
 <body>
+    <?php  include_once 'views/header.php'; ?>
     <h1>Hola</h1>
     <script src="assets/js/script.js"></script>
 </body>
---
+++ views/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+    <div class="header-1">
+        <a href="#" class="logo">
+            <i class="fas fa-book">
+                Book Store
+            </i>
+        </a>
+        <form action="" class="search-form">
+            <input type="search" name="" placeholder="Busca un título..." class="search-box" id="search-box">
+            <label for="search-box" class="fas fa-search"></label>
+        </form>
+    
+        <div class="icons">
+            <div id="search-btn" class="fas fa-search">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <div id="login-btn" class="fas fa-user"></div>
+            </div>
+        </div>
+    </div>
+</header>
